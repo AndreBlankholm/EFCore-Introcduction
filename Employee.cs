e7b7cd6d8365975b31e3e0e45e1dba67f8c0b48f
Andre Blankholm <ablankholm@gmail.com>
2025-11-04 15:00:56 -0600

    added EFCore package and Employee class with properties

---
+++ Employee.cs
@@ -0,0 +1,12 @@
+
+    namespace Name
+    {
+        public class Employee
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Position { get; set; }
+            public DateTime HireDate { get; set; }
+            
+        }
+    }
