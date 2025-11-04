c41ac67539dbf2716ae6cc375bfc63b439d58195
Andre Blankholm <ablankholm@gmail.com>
2025-11-04 17:52:12 -0600

    added employyes to program and made a variable to extract all employees that are in the Huma Resorces Department

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EFCore_Introcduction;
+using Microsoft.EntityFrameworkCore;
+
+var context = new CompanyContext();
+
+context.Departments.Add(
+new Department
+{
+    Name = "Human Resources",
+    Employees = new List<Employee>
+    {
+        new Employee
+        {
+            Name = "Alice Johnson",
+            Position = "HR Manager",
+            HireDate = new DateTime(2020, 1, 15)
+        },
+        new Employee
+        {
+            Name = "Bob Smith",
+            Position = "Recruiter",
+            HireDate = new DateTime(2021, 6, 10)
+        }
+    }
+});
+
+context.SaveChanges();
+
+var employees = context.Employees.Where(e => e.Department.Name == "Human Resources").ToList();
+Console.WriteLine($"Employees in Human Resources Department: {employees.Count}");
